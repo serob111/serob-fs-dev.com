7ee0b8ee9ecadec1cdfc8e10684803d8656d962f
serob111 <serobkhurshudyan111@gmail.com>
2026-02-19 01:59:09 +0400

    fixes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://serob111.github.io/serob-fs-dev.com/assets/Gauge.png",
+  base: "https://serob111.github.io/serob-fs-dev.com/assets/",
   plugins: [react()],
   publicDir: "public"
 });
