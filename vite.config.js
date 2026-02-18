6234410ff541efbd29c75dcf54ba21cbc7724260
serob111 <serobkhurshudyan111@gmail.com>
2026-02-19 01:52:27 +0400

    fixes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/serob-fs-dev.com/",
   plugins: [react()],
   publicDir: "public"
 });
